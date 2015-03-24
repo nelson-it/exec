62360f46121b8e9e9d64f112a913b9230f330f6e
Manfred Nelson <manny@puran2.ch>
2015-03-24 09:28:06 +0100

    Version 3.5 fertiggestellt

---
+++ system/shell/apache2/webdav/mneerp/read.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+. exec/system/shell/allg/db.sh
+
+getreleases="SELECT name, location, description FROM mne_repository.folder;"
+get_releases()
+{
+    par=$1
+    name=${par/|*};        par=${par#*|}
+    location=${par/|*};    par=${par#*|}
+    description=${par/|*}; par=${par#*|}
+}
