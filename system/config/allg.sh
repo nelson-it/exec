5ec822508f624aabfc9a9131c344f0b423364726
Nelson - technische Informatik <entwicklung@nelson-it.ch>
2015-06-15 09:44:04 +0200

    Netwerkkonfiguration hinzugefügt, Webdav verbessert

--- system/config/allg.sh
+++ system/config/allg.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
 
-DBUSER=mneerprepository
+function mne_error_handler()
+{
+    # Aufruf mit: trap 'mne_error_handler $BASH_COMMAND $LINENO $?' ERR
+
+	if [ "$mne_error_ignore" = "1" ]; then
+		return;
+	fi
+
+	if [ "$mne_error_need" = "" ]; then
+	    cmd=`echo $1 | cut "-d " -f 1`
+        if [ "$cmd" = "fgrep" ]; then
+		 return;
+		fi
+	fi
+    
+    echo "error in: $0" 1>&2
+    echo "     cmd: $1" 1>&2
+    echo "    line: $2" 1>&2
+    echo "  status: $3" 1>&2
+    exit 1
+}
+
+trap 'mne_error_handler "$BASH_COMMAND" $LINENO $?' ERR
+exit_status=0
+
+export LANG=C
+
+DBUSER=mneerpsystem
 DB=erpdb
 
 IMPL=mneerp
 DATAROOT=`pwd`/data
 
 SYSVERSION=default
-
 fgrep 12.04 /etc/issue 2>&1 > /dev/null
 if [ "$?" = "0" ]; then
     SYSVERSION=12_04
@@ -16,8 +42,12 @@ if [ "$?" = "0" ]; then
 fgrep 14.04 /etc/issue 2>&1 > /dev/null
 if [ "$?" = "0" ]; then
     SYSVERSION=14_04
- fi
+fi
  
+if [ -f "exec/local/system/config/$SYSVERSION/config.sh" ]; then
+  . exec/local/system/config/$SYSVERSION/config.sh
+fi
+
 if [ -f "exec/system/config/$SYSVERSION/config.sh" ]; then
   . exec/system/config/$SYSVERSION/config.sh
 fi
--- system/shell/allg/file.sh
+++ system/shell/allg/file.sh
@@ -2,8 +2,27 @@
 
 function find_template()
 {
+   if [ -f "exec/local/system/templates/$SYSVERSION/$2/$IMPL/$3" ]; then
+     eval $1=exec/local/system/templates/$SYSVERSION/$2/$IMPL/$3;
+     return
+   fi
+   
+   if [ -f "exec/local/system/templates/default/$2/$IMPL/$3" ]; then
+     eval $1=exec/local/system/templates/default/$2/$IMPL/$3;
+     return
+   fi
+   
+   if [ -f "exec/local/system/templates/$SYSVERSION/$2/$3" ]; then
+     eval $1=exec/local/system/templates/$SYSVERSION/$2/$3;
+     return
+   fi
+   
+   if [ -f "exec/local/system/templates/default/$2/$3" ]; then
+     eval $1=exec/local/system/templates/default/$2/$3;
+     return
+   fi
    if [ -f "exec/system/templates/$SYSVERSION/$2/$IMPL/$3" ]; then
-     eval $1=exec/system/templates/$SYSVERSION/$2/$IMPL/$3;
+      eval $1=exec/system/templates/$SYSVERSION/$2/$IMPL/$3;
      return
    fi
    
@@ -12,16 +31,62 @@ function find_template()
      return
    fi
    
-   if [ -f "exec/system/templates/$SYSVERSION/$2/default/$3" ]; then
-     eval $1=exec/system/templates/$SYSVERSION/$2/default/$3;
+   if [ -f "exec/system/templates/$SYSVERSION/$2/$3" ]; then
+     eval $1=exec/system/templates/$SYSVERSION/$2$3;
      return
    fi
    
-   if [ -f "exec/system/templates/default/$2/default/$3" ]; then
-     eval $1=exec/system/templates/default/$2/default/$3;
+   if [ -f "exec/system/templates/default/$2/$3" ]; then
+     eval $1=exec/system/templates/default/$2/$3;
      return
    fi
    
    echo "File not found $2 $3" 1>&2 
+   exit -1
+} 
+
+function find_script()
+{
+   if [ -f "exec/local/system/shell/$SYSVERSION/$1/$IMPL/$2" ]; then
+    . exec/local/system/shell/$SYSVERSION/$12/$IMPL/$2;
+     return
+   fi
+
+   if [ -f "exec/local/system/shell/$SYSVERSION/$1/$2" ]; then
+     . exec/local/system/shell/$SYSVERSION/$1/$2;
+     return
+   fi
+
+   if [ -f "exec/local/system/shell/$1/$IMPL/$2" ]; then
+     . exec/local/system/shell/$1/$IMPL/$2;
+     return
+   fi
+
+   if [ -f "exec/local/system/shell/$1/$2" ]; then
+     . exec/local/system/shell/$1/$2;
+     return
+   fi
+
+   if [ -f "exec/system/shell/$SYSVERSION/$1/$IMPL/$2" ]; then
+     . exec/system/shell/$SYSVERSION/$1/$IMPL/$2;
+     return
+   fi
+
+   if [ -f "exec/system/shell/$SYSVERSION/$1/$2" ]; then
+     . exec/system/shell/$SYSVERSION/$1/$2;
+     return
+   fi
+
+   if [ -f "exec/system/shell/$1/$IMPL/$2" ]; then
+     . exec/system/shell/$1/$IMPL/$2;
+     return
+   fi
+
+   if [ -f "exec/system/shell/$1/$2" ]; then
+     . exec/system/shell/$1/$2;
+     return
+   fi
+   
+   echo "File not found $1 $2" 1>&2 
    exit -1
 } 
