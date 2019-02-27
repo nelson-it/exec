faf9ea3a5ce785017dff7c806e06b4c26a7de3f0
Nelson - technische Informatik <entwicklung@nelson-it.ch>
2019-02-27 16:57:17 +0100

    Erste Version asynchrone Kommunikation

--- system/shell/domain/share/mneerp/read.sh
+++ system/shell/domain/share/mneerp/read.sh
@@ -11,6 +11,15 @@ getreleases="SELECT DISTINCT t0.name, t0.location, t0.description \
               WHERE t1.folderid IS NOT NULL AND t2.loginname != '' AND t4.group IS NOT NULL \
            ORDER BY t0.name"
 
+getrelease="SELECT DISTINCT t0.name, t0.location, t0.description \
+           FROM mne_application.folder t0 \
+             LEFT JOIN mne_system.shares t1 ON ( t0.folderid = t1.folderid ) \
+             LEFT JOIN mne_crm.personowndata t2 ON ( t1.personid = t2.personid ) \
+             LEFT JOIN pg_catalog.pg_roles t3 ON ( t2.loginname = t3.rolname)\
+             LEFT JOIN mne_catalog.accessgroup t4 ON ( t3.rolname = t4.member AND t4.group = 'erpsmb' )\
+              WHERE t1.folderid IS NOT NULL AND t2.loginname != '' AND t4.group IS NOT NULL AND t1.sharesid ='par1' \
+           ORDER BY t0.name"
+
 get_releases()
 {
     par=$1
