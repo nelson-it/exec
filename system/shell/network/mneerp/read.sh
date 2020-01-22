33cc5d7340dd81a789c18c757bfb9f3ccec2d5ce
Nelson - technische Informatik <entwicklung@nelson-it.ch>
2020-01-22 14:19:30 +0100

    Netzwerk Einstellungen überarbeitet

--- system/shell/network/mneerp/read.sh
+++ system/shell/network/mneerp/read.sh
@@ -2,8 +2,23 @@
 
 . exec/system/shell/allg/db.sh
 
-getinterfaces="SELECT t0.networkid, t0.typ, t0.addr, t0.mask, t0.addr6, t0.mask6, t0.broadcast, t0.gateway, t0.nameserver, COALESCE(t1.domain, t0.domain), t0.search FROM mne_system.network t0 LEFT JOIN mne_system.domain t1 ON ( t0.hostname = t1.domainid ) WHERE hostname ='$(hostname)' ORDER BY networkid;"
-getsingleinterface="SELECT t0.networkid, t0.typ, t0.addr, t0.mask, t0.addr6, t0.mask6, t0.broadcast, t0.gateway, t0.nameserver, COALESCE(t1.domain, t0.domain), t0.search FROM mne_system.network t0 LEFT JOIN mne_system.domain t1 ON ( t0.hostname = t1.domainid ) WHERE hostname = '$(hostname)' AND networkid = 'par1';"
+getinterfaces="SELECT t0.networkid, t0.typ, t0.addr, t0.mask, 
+                      t0.addr6, t0.mask6, t0.broadcast, t0.gateway,
+                      CASE WHEN t1.typ = E'standalone' THEN t0.nameserver ELSE COALESCE(NULLIF(t2.addr6,''), NULLIF(t2.addr,''), E'127.0.0.1') END,                      COALESCE(t1.domain, t0.domain),
+                      CASE WHEN t1.typ = 'standalone' THEN t0.search ELSE t1.dnssearch END
+               FROM mne_system.network t0 
+                 LEFT JOIN mne_system.domain t1 ON ( t0.hostname = t1.domainid )
+                 LEFT JOIN mne_system.network t2 ON ( t2.hostname = t1.domainid AND t2.networkid = t1.netdevice )
+               WHERE t0.hostname ='$(hostname)' ORDER BY networkid;"
+               
+getsingleinterface="SELECT t0.networkid, t0.typ, t0.addr, t0.mask, 
+                      t0.addr6, t0.mask6, t0.broadcast, t0.gateway,
+                      CASE WHEN t1.typ = E'standalone' THEN t0.nameserver ELSE COALESCE(NULLIF(t2.addr6,''), NULLIF(t2.addr,''), E'127.0.0.1') END,                      COALESCE(t1.domain, t0.domain),
+                      CASE WHEN t1.typ = 'standalone' THEN t0.search ELSE t1.dnssearch END
+               FROM mne_system.network t0 
+                 LEFT JOIN mne_system.domain t1 ON ( t0.hostname = t1.domainid )
+                 LEFT JOIN mne_system.network t2 ON ( t2.hostname = t1.domainid AND t2.networkid = t1.netdevice )
+               WHERE t0.hostname = '$(hostname)' AND t0.networkid = 'par1';"
 
 get_interfaces()
 {
