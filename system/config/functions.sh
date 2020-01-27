653a330c07b1c11a946d7552db4818af4b02ef8d
Nelson - technische Informatik <entwicklung@nelson-it.ch>
2020-01-27 12:56:10 +0100

    Netzwerk reslov.conf angepasst die erste

--- system/config/functions.sh
+++ system/config/functions.sh
@@ -51,11 +51,11 @@ function cleanup
 {
   ifconfig lo:mneconfig 127.0.156.1 netmask 255.255.255.0 down
   
-  echo End ===================================================================== 1>&$logfile
   if [ "$needreboot" != "" ]; then
       echo "reboot in 1 min"
       shutdown -r 1>&$logfile 2>&$logfile
   fi
+  echo End ===================================================================== 1>&$logfile
   sleep 0
 }
 
